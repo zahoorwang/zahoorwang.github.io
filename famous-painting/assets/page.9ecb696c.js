43166256bcd010bfa7ae14cb7b2c5f0c7d11ee74
Zahoor Wang <zahoor.wang@gmail.com>
2022-02-06 15:06:15 +0800

    -feat: Added the FamousPainting demo pages.

---
+++ famous-painting/assets/page.9ecb696c.js
@@ -0,0 +1,1 @@
+import{_ as s,a as r,b as c,c as l}from"./app.50bd51a9.js";import{C as p,o as u,b as m,j as e,f as n,F as i}from"./vendor.ebb0b43e.js";const d={},f=n("footer",null,null,-1);function v(g,b){const o=p("router-view"),_=r,t=c,a=l;return u(),m(i,null,[e(o),n("header",null,[e(_),e(t)]),n("main",null,[e(a)]),f],64)}var j=s(d,[["render",v]]);export{j as default};
