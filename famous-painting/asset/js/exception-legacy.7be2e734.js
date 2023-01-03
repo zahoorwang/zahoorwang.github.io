4b074bd146f5caf3dab302b64a9be3e17c8199a4
Zahoor Wang <zahoor.wang@gmail.com>
2023-01-03 10:09:24 +0800

    -update: Deleted the famous-painting folder.

--- famous-painting/asset/js/exception-legacy.7be2e734.js
+++
@@ -1,1 +0,0 @@
-System.register(["./vendor-legacy.ffeea19c.js"],(function(e){"use strict";var t,n,r,u;return{setters:[function(e){t=e.h,n=e.i,r=e.o,u=e.j}],execute:function(){var c={class:"exception"};e("default",t({setup:function(e){return n(),function(e,t){return r(),u("div",c)}}}))}}}));
--- famous-painting/asset/js/index.4ecb2158.js
+++
@@ -1,1 +0,0 @@
-import{b as o}from"./index.ee78c849.js";import"./vendor.b017440c.js";const t={name:"Category"};typeof o=="function"&&o(t);export{t as default};
